b41554a3529e0c1094c79400a674eafc5dfeaf6a
Sofi <sofi.samorodova@gmail.com>
2021-04-14 22:31:08 +0300

    new classes for ui

--- src/mainFrame.java
+++ src/mainFrame.java
@@ -90,12 +90,12 @@ public class mainFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
+    /*public static void main(String args[]) {
+        *//* Set the Nimbus look and feel *//*
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+        *//* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
+         *//*
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -115,13 +115,13 @@ public class mainFrame {
         //</editor-fold>
         //</editor-fold>
 
-        /* Create and display the form */
+        *//* Create and display the form *//*
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new mainFrame().setVisible(true);
             }
         });
-    }
+    }*/
 
     // Variables declaration - do not modify
     private javax.swing.JButton jButton1;
